9238fa96d2f305ffa3fb9bcf02adab31bfbb3a10
Stylo27 <styloff@gmail.com>
2017-08-05 18:12:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+$(function () {
+    var data = $( "#create_form" ).submit(function(event){
+        // var userName = $("#name").val();
+        // var userSurname = $("#surname").val();
+        // var userAge = $("#age").val();
+        // var userGender = $("input:radio:checked").val();
+        // var userHobby = $("input:checkbox:checked");
+        // var a = userHobby.map(function(element){
+        //     return {name : element.name,
+        //             value : element.value
+        //     }
+        // });
+        // console.log(userName);
+        // console.log(userSurname);
+        // console.log(userAge);
+        // console.log(userGender);
+        // console.log(a)
+    });
+
+    console.log(data)
+
+});
