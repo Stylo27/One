716bcd3c7eea2e3e3e6fe9bac130e640ec47efb8
Stylo27 <styloff@gmail.com>
2017-08-07 13:05:04 +0300

    add iterLocalStorage

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
+function createUserRow(index, user) {
+    if (user.hobby != []){
+        var userhobby = user.hobby.join()
+    } else {
+        userhobby = text("No hobby")
+    }
+    var user_row = $("<tr />").append([
+        $("<td />").text(index),
+        $("<td />").text(user.user_name),
+        $("<td />").text(user.user_surname),
+        $("<td />").text(user.user_age),
+        $("<td />").text(user.gender),
+        $("<td />").text(userhobby),
+        $("<td />").text(user.country)
+    ])
+
+    $("table#users_table tbody").append(user_row)
+};
+
+function iterLocalStorage() {
+    var users = JSON.parse(localStorage.getItem('users') || '[]');
+    for(var k = 0, length3 = users.length; k < length3; k++){
+        console.log(users[k]);
+        createUserRow(k ,users[k])
+    }
+    
+
+}
+
+// iterLocalStorage();
+
 $(function(){
     $('#create_form').on('submit', function (e) {
         e.preventDefault();
         var form = $(e.target);
         serial_form = form.serializeArray();
+        console.log('serial_form : ',serial_form)
         userObj = new Object()
         for(var k = 0, length3 = serial_form.length; k < length3; k++){
             if(serial_form[k].name === "hobby"){
@@ -16,31 +48,17 @@ $(function(){
                 userObj[serial_form[k].name] = serial_form[k].value;
             }
         }
-        console.log(userObj);
         createUserRow('0', userObj); //Вот тут добавил вызов
         json_form = JSON.stringify(userObj);
-        
+  
+
         var users = JSON.parse(localStorage.getItem('users') || '[]');
         users.push(json_form);
         localStorage.setItem("users", JSON.stringify(users));
-        console.log(users)
+        console.log('users :',users)
 
-        console.log(userObj.user_name)
     });
 
 });
   
     
-function createUserRow(index, user) {
-    var user_row = $("<tr />").append([
-        $("<td />").text(index),
-        $("<td />").text(user.user_name),
-        $("<td />").text(user.user_surname),
-        $("<td />").text(user.user_age),
-        $("<td />").text(user.gender),
-        $("<td />").text(user.hobby.join(",")),
-        $("<td />").text(user.country)
-    ])
-
-    $("table#users_table tbody").append(user_row)
-};
